34ee3bce5ee19722440599c57131d87a19069b52
James Hillyerd <james@hillyerd.com>
2021-01-02 22:08:32 -0800

    blink scope via TIM2

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use embedded_hal::digital::v2::*;
 use rtic::app;
 use rtic::cyccnt::{Duration, U32Ext};
 use rtt_target::rprintln;
-use stm32f1xx_hal::{gpio::*, prelude::*};
+use stm32f1xx_hal::{gpio::*, pac, prelude::*, rcc::Clocks, timer};
 
 const SYSCLK: u32 = 72_000_000;
 const BUTTON_POLL_PERIOD: u32 = SYSCLK / 100; // Hz
@@ -18,6 +18,7 @@ const APP: () = {
     struct Resources {
         #[init(1)]
         steps: u32,
+        tim2: timer::CountDownTimer<pac::TIM2>,
         led: gpioc::PC13<Output<PushPull>>,
         button: gpioa::PA10<Input<PullUp>>,
         button_state: Debouncer<u8, debouncr::Repeat6>,
@@ -37,7 +38,7 @@ const APP: () = {
         // Setup and apply clock confiugration.
         let mut flash = dp.FLASH.constrain();
         let mut rcc = dp.RCC.constrain();
-        let clocks = rcc
+        let clocks: Clocks = rcc
             .cfgr
             .use_hse(8.mhz())
             .sysclk(SYSCLK.hz())
@@ -46,11 +47,16 @@ const APP: () = {
         rprintln!(" SYSCLK: {:?} MHz", clocks.sysclk().0 / 1_000_000);
         rprintln!(" HCLK: {:?} MHz", clocks.hclk().0 / 1_000_000);
         rprintln!(" APB1 clk: {:?} MHz", clocks.pclk1().0 / 1_000_000);
+        rprintln!(" APB1 TIM: {:?} MHz", clocks.pclk1_tim().0 / 1_000_000);
         rprintln!(" APB2 clk: {:?} MHz", clocks.pclk2().0 / 1_000_000);
         rprintln!(" ADCCLK: {:?} MHz", clocks.adcclk().0 / 1_000_000);
 
+        // Timer setup.
+        let mut tim2: timer::CountDownTimer<pac::TIM2> =
+            timer::Timer::tim2(dp.TIM2, &clocks, &mut rcc.apb1).start_count_down(2.khz());
+        tim2.listen(timer::Event::Update);
+
         // Peripheral setup.
-        let _afio = dp.AFIO.constrain(&mut rcc.apb2);
         let mut gpioa = dp.GPIOA.split(&mut rcc.apb2);
         let mut gpioc = dp.GPIOC.split(&mut rcc.apb2);
 
@@ -64,9 +70,6 @@ const APP: () = {
 
         // Setup pa10 button interrupt.
         let button = gpioa.pa10.into_pull_up_input(&mut gpioa.crh);
-        // button.make_interrupt_source(&mut afio);
-        // button.trigger_on_edge(&dp.EXTI, Edge::FALLING);
-        // button.enable_interrupt(&dp.EXTI);
 
         cx.spawn.poll_button().unwrap();
         cx.spawn.blink_led().unwrap();
@@ -85,28 +88,31 @@ const APP: () = {
 
         init::LateResources {
             led,
+            tim2,
             button,
             button_state: debouncr::debounce_6(false),
             scope,
         }
     }
 
-    #[task(resources = [steps, led, scope], schedule = [blink_led])]
+    #[task(resources = [steps, led], schedule = [blink_led])]
     fn blink_led(cx: blink_led::Context) {
-        // TODO schedule using timer resource.
-        let blink_led::Resources {
-            mut steps,
-            led,
-            scope,
-        } = cx.resources;
+        let blink_led::Resources { mut steps, led } = cx.resources;
 
         led.toggle().unwrap();
-        scope.toggle().unwrap();
 
         // Schedule next blink.
         let steps = steps.lock(|s| *s);
         let delay = Duration::from_cycles(CYCLES_PER_STEP * steps);
         cx.schedule.blink_led(cx.scheduled + delay).unwrap();
+    }
+
+    #[task(binds = TIM2, priority = 3, resources = [tim2, scope])]
+    fn toggle_scope(cx: toggle_scope::Context) {
+        let toggle_scope::Resources { tim2, scope } = cx.resources;
+
+        scope.toggle().unwrap();
+        tim2.clear_update_interrupt_flag();
     }
 
     #[task(
